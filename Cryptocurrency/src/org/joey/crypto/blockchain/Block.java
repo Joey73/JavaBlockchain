8228070939f91e9f81860e77a8610a23db1c27ce
Joey73 <schmitz873@gmail.com>
2018-09-15 14:34:45 +0200

    Several changes

--- Cryptocurrency/src/org/joey/crypto/blockchain/Block.java
+++ Cryptocurrency/src/org/joey/crypto/blockchain/Block.java
@@ -1,5 +1,68 @@
 package org.joey.crypto.blockchain;
 
-public class Block {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import org.joey.crypto.constants.Constants;
+import org.joey.crypto.cryptocurrency.CryptographyHelper;
+import org.joey.crypto.cryptocurrency.Transaction;
+
+public class Block {
+	private int id;
+	private int nonce;
+	private long timeStamp;
+	private String hash;
+	private String previousHash;
+	private List<Transaction> transactions;
+
+	public Block(String previousHash) {
+		super();
+		this.transactions = new ArrayList<Transaction>();
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		this.generateHash();
+	}
+
+	public void generateHash() {
+		String dataToHash = Integer.toString(this.id) //
+				+ this.previousHash //
+				+ Long.toString(this.timeStamp) //
+				+ Integer.toString(this.nonce) //
+				+ this.transactions.toString();
+		String hashValue = CryptographyHelper.generateHash(dataToHash);
+		this.hash = hashValue;
+	}
+
+	public void incrementNonce() {
+		this.nonce++;
+	}
+
+	public String getHash() {
+		return this.hash;
+	}
+
+	// check if the given transaction is valid or not
+	public boolean addTransaction(Transaction transaction) {
+		if(transaction == null) {
+			return false;
+		}
+		
+		// if the block is the genesis block we do not process
+		if(!previousHash.equals(Constants.GENESIS_PREV_HASH)) {
+			if(!transaction.verifyTransaction()) {
+				System.out.println("Transaction is not valid...");
+				return false;
+			}
+		}
+		
+		transactions.add(transaction);
+		System.out.println("Transaction is valid and it's added to the block" + this);
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Block [id=" + this.id + ", hash=" + this.hash + ", previousHash=" + this.previousHash + ", transaction=" + this.transactions.toString() + "]";
+	}
 }
--- Cryptocurrency/src/org/joey/crypto/cryptocurrency/Wallet.java
+++ Cryptocurrency/src/org/joey/crypto/cryptocurrency/Wallet.java
@@ -1,5 +1,66 @@
 package org.joey.crypto.cryptocurrency;
 
-public class Wallet {
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import org.joey.crypto.blockchain.Blockchain;
+
+public class Wallet {
+	private PrivateKey privateKey;
+	private PublicKey publicKey;
+	
+	public Wallet() {
+		KeyPair keyPair = CryptographyHelper.ellipticCurveCrypto();
+		this.privateKey = keyPair.getPrivate();
+		this.publicKey = keyPair.getPublic();
+	}
+	
+	public double calculateBalance() {
+		double balance = 0;
+		for(Map.Entry<String, TransactionOutput> item : Blockchain.UTXOs.entrySet()) {
+			TransactionOutput transactionOutput = item.getValue();
+			if(transactionOutput.isMine(publicKey)) {
+				balance += transactionOutput.getAmount();
+			}
+		}
+		return balance;
+	}
+	
+	public Transaction transferMoney(PublicKey receiver, double amount) {
+		if(calculateBalance() < amount) {
+			System.out.println("Invalid transaction because of not enough money...");
+			return null;
+		}
+		
+		// we store the inputs for the transaction in this array
+		List<TransactionInput> inputs = new ArrayList<TransactionInput>();
+		
+		// let's find out unspent transactions (the blockchain stores all the UTXOs)
+		for(Map.Entry<String, TransactionOutput> item : Blockchain.UTXOs.entrySet()) {
+			TransactionOutput UTXO = item.getValue();
+			
+			if(UTXO.isMine(this.publicKey)) {
+				inputs.add(new TransactionInput(UTXO.getId()));
+			}
+		}
+		
+		// let's create the new transaction
+		Transaction newTransaction = new Transaction(publicKey, receiver, amount, inputs);
+		// the sender signs the transaction
+		newTransaction.generateSignature(privateKey);
+		
+		return newTransaction;
+	}
+	
+	public PublicKey getPublicKey() {
+		return this.publicKey;
+	}
+
+	public PrivateKey getPrivateKey() {
+		return this.privateKey;
+	}
 }
